592181a470c8e0616d749ea27a70d8d9900be0a6
Desma <dhervey@basecampcodingacademy.org>
2018-07-09 16:20:43 -0500

    almost done

---
+++ core.py
@@ -0,0 +1,37 @@
+def new_gladiator(health, rage, damage_low, damage_high):
+    '''(int, int, int int) -> {Dictionary}
+    Returns a dictionary with 4 key-value pairs.
+    return gladiator
+    '''
+    gladiator = {
+        'health': health,
+        'rage': rage,
+        'damage_low': damage_low,
+        'damage_high': damage_high
+    }
+    return gladiator
+
+def attack(attacker, defender):
+
+
+
+
+
+def heal(gladiator):
+
+
+
+
+
+
+
+def is_dead(gladiator):
+    
+
+
+
+
+
+
+
+
